b4429324c2fbf635588eccf084e45886f81332e7
Melinda <codegirl78@gmail.com>
2021-06-21 14:30:14 -0500

    Changed text color for heading and list items to purple, centered movie list heading, made font size large enough to read in movie-list.component.css, changed font to Arial Narrow Bold so that it's large enough to read, changed text color to hot pink, underlined chores list heading, and italicized chores in list for chores-list.component.css, added 2 additional items to the movies array, added 2 more li elements to the movie-list.component.html and used palceholders to reference the new movies in the array, and in the FavPhotoComponent, assigned a better section heading to the photosTitle variable, added 2 additional image URLs, used placeholders in the img tags to display chosen images, adjusted HTML to display one image per line, and used css file to make all images be the same size

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'My Awesome Images';
   image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://images.freeimages.com/images/large-previews/322/indian-heads-1391201.jpg';
+  image3 = 'https://images.pexels.com/photos/2662116/pexels-photo-2662116.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500';
 
   constructor() { }
 
