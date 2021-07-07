dced4c9a17e7d05d4c7ec49d8e7918eb981f31e4
Melinda <codegirl78@gmail.com>
2021-07-07 11:38:01 -0500

    Working on completing bonus

--- lesson3/examples/src/app/buttons/buttons.component.ts
+++ lesson3/examples/src/app/buttons/buttons.component.ts
@@ -7,10 +7,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ButtonsComponent implements OnInit {
    buttonHeading: string = "Buttons"
-   inactive: boolean = false;
+   inactiveG: boolean = false;
+   inactiveS: boolean = false;
+   inactiveC: boolean = false;
+   inactiveY: boolean = true;
+
+   visibleMe = false;
+   visibleMe1 = true;
+   visibleMe2 = true;
 
    constructor() { }
 
    ngOnInit() { }
+
+   inac = () => {
+      this.inactiveG = false;
+      this.inactiveS = false;
+      this.inactiveC = false;
+   }
+
+   catchY = () => {
+      this.inactiveY = false;
+   }
+
+   releaseY = () => {
+      this.inactiveY = true;
+   }
+
+   catchMe1 = () => {
+      this.visibleMe = true;
+      this.visibleMe1 = false;
+      this.visibleMe2 = true;
+   }
+
+   catchMe2 = () => {
+      this.visibleMe = false;
+      this.visibleMe1 = true;
+      this.visibleMe2 = true;
+   }
+   
+   catchMe3 = () => {
+      this.visibleMe = true;
+      this.visibleMe1 = true;
+      this.visibleMe2 = false;
+   }
 
 }
